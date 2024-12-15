ed454e9a1f73079b8e75ad8a5d011aae89e17f76
Cody Jandes <CMJandes@student.fullsail.edu>
2024-12-14 23:05:30 -0500

    Updated Keyboard Menu Function
    -Updated keyboard function to switch between mouse and KB

--- Domasis_Defied_Duel/Assets/Cody Jandes's Sandbox/Assets/Scripts/DefaultButtonControl.cs
+++ Domasis_Defied_Duel/Assets/Cody Jandes's Sandbox/Assets/Scripts/DefaultButtonControl.cs
@@ -3,20 +3,55 @@ using UnityEngine.EventSystems;
 
 public class DefaultButtonControl : MonoBehaviour
 {
-
+    //Set default button
     [SerializeField] private GameObject defaultButton;
+
+    private GameObject lastSelected;
+    private bool isUsingMouse = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
-        //Make sure events chooses the right button 
-        EventSystem.current.SetSelectedGameObject(null); //Clear Previous
-        EventSystem.current.SetSelectedGameObject(defaultButton); //Set default
+        //Set default on start 
+        SetDefaultButton();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Handle mouse if clicked
+        if (Input.GetMouseButtonDown(0))
+        {
+            isUsingMouse = true;
+        }
+
+        //Handle Arrow keys 
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            //turn mouse off
+            isUsingMouse= false;
+
+            //If no button selected
+            if (EventSystem.current.currentSelectedGameObject == null)
+            {
+                EventSystem.current.SetSelectedGameObject(lastSelected ?? defaultButton);
+            }
+        }
+
+        //If button is selected
+        if (EventSystem.current.currentSelectedGameObject != null)
+        {
+            lastSelected = EventSystem.current.currentSelectedGameObject;
+        }
+    }
+
+    private void SetDefaultButton()
+    {
+        //Make sure events chooses the right button 
+        EventSystem.current.SetSelectedGameObject(null); //Clear Previous
+        EventSystem.current.SetSelectedGameObject(defaultButton); //Set default
+
+        //Save default as last 
+        lastSelected = defaultButton;
     }
 }
